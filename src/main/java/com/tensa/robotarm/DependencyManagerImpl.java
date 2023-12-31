e98e9c503198ce8d04a33432e8dc40933b9bd302
10sa <zenkins52@gmail.com>
2023-12-31 14:44:44 +0900

    Implementation DependencyManager interface

---
+++ src/main/java/com/tensa/robotarm/DependencyManagerImpl.java
@@ -0,0 +1,91 @@
+package com.tensa.robotarm;
+
+import com.tensa.robotarm.annotation.Injection;
+import lombok.NonNull;
+import lombok.val;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DependencyManagerImpl implements DependencyManager {
+	
+	protected final List<DependencyScanner> scanners = new LinkedList<>();
+	
+	@NonNull
+	protected final DependencyPool dependencyPool;
+	
+	public DependencyManagerImpl(@NonNull final DependencyPool dependencyPool) {
+		this.dependencyPool = dependencyPool;
+	}
+	
+	public void initialize() {
+		val dependencies = new LinkedList<DependencyScanner.ScanResult>();
+		for (val scanner : scanners)
+			dependencies.addAll(scanner.scan());
+		
+		dependencies.sort(Comparator.comparing(v -> ((DependencyScanner.ScanResult)v).getAnnotation().priority()).reversed());
+		
+		for (val dependencyInfo : dependencies) {
+			val annotation = dependencyInfo.getAnnotation();
+			final Class<?> registerClass = annotation.clazz().equals(Void.class) ? dependencyInfo.getClazz() : annotation.clazz();
+			
+			try {
+				dependencyPool.register(dependencyInfo.getClazz().newInstance(), annotation.value(), registerClass);
+			}
+			catch (Exception e) {
+				throw new IllegalStateException("Failed to create dependency instance.", e);
+			}
+		}
+		
+		for (val dependency : dependencyPool.getAllDependencies())
+			inject(dependency.getObject());
+	}
+	
+	public void addDependencyScanner(@NonNull final DependencyScanner scanner) {
+		scanners.add(scanner);
+	}
+	
+	public List<DependencyScanner> getScanners() {
+		return Collections.unmodifiableList(scanners);
+	}
+	
+	@Override
+	public void inject(@NonNull final Object object) {
+		for (val field : object.getClass().getDeclaredFields()) {
+			val injectInfo = field.getAnnotation(Injection.class);
+			if (injectInfo != null)
+				injectDependency(object, field, injectInfo);
+		}
+	}
+	
+	@Override
+	public @NonNull DependencyPool getDependencyPool() {
+		return dependencyPool;
+	}
+	
+	protected void injectDependency(final Object object, final Field field, final Injection injectInfo) {
+		val name = injectInfo.value();
+		final Class<?> clazz = injectInfo.clazz().equals(Void.class) ? field.getType() : injectInfo.clazz();
+		
+		val dep = dependencyPool.getDependency(name, clazz);
+		if (injectInfo.required() && !dep.isPresent())
+			throw new DependencyUnsatisfiedException();
+		
+		dep.ifPresent((v -> setField(object, field, v)));
+	}
+	
+	private static void setField(final Object object, final Field field, final Object value) {
+		try {
+			field.setAccessible(true);
+			field.set(object, value);
+			field.setAccessible(false);
+		}
+		catch (Exception e) {
+			throw new DependencyException(e);
+		}
+	}
+	
+}
---
+++ src/test/java/com/tensa/robotarm/DependencyManagerImplTest.java
@@ -0,0 +1,97 @@
+package com.tensa.robotarm;
+
+import com.tensa.robotarm.annotation.Dependency;
+import com.tensa.robotarm.annotation.Injection;
+import lombok.SneakyThrows;
+import lombok.val;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.description.annotation.AnnotationDescription;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DependencyManagerImplTest {
+	
+	@SneakyThrows
+	@Test
+	void initializeTest() {
+		val manager = new DependencyManagerImpl(new HashSetDependencyPool());
+		
+		val dependency1 = new ByteBuddy()
+			.subclass(Object.class)
+			.annotateType(AnnotationDescription.Builder.ofType(Dependency.class).build())
+			.make()
+			.load(getClass().getClassLoader(), ClassLoadingStrategy.Default.INJECTION)
+			.getLoaded();
+		
+		val dependency2 = new ByteBuddy()
+			.subclass(Object.class)
+			.annotateType(AnnotationDescription.Builder.ofType(Dependency.class)
+				.define("clazz", Object.class)
+				.define("value", "dependency2-name")
+				.build())
+			.make()
+			.load(getClass().getClassLoader(), ClassLoadingStrategy.Default.INJECTION)
+			.getLoaded();
+		
+		val injectionTarget = new ByteBuddy()
+			.subclass(Object.class)
+			.annotateType(AnnotationDescription.Builder.ofType(Dependency.class).build())
+			.defineField("dependency1", dependency1, Modifier.PUBLIC)
+			.annotateField(AnnotationDescription.Builder.ofType(Injection.class).build())
+			.defineField("dependency2", Object.class, Modifier.PUBLIC)
+			.annotateField(AnnotationDescription.Builder.ofType(Injection.class)
+				.define("clazz", Object.class)
+				.define("value", "dependency2-name")
+				.build())
+			.make()
+			.load(getClass().getClassLoader())
+			.getLoaded();
+		
+		manager.addDependencyScanner(() -> Arrays.asList(
+			new DependencyScanner.ScanResult(dependency1, dependency1.getAnnotation(Dependency.class)),
+			new DependencyScanner.ScanResult(dependency2, dependency2.getAnnotation(Dependency.class)),
+			new DependencyScanner.ScanResult(injectionTarget, injectionTarget.getAnnotation(Dependency.class))
+		));
+		
+		manager.initialize();
+		
+		val dependencies = manager.getDependencyPool().getDependencies(injectionTarget);
+		assertEquals(1, dependencies.size());
+		assertEquals(injectionTarget, dependencies.get(0).getClass());
+		
+		val dependency = dependencies.get(0);
+		assertEquals(dependency1, dependency.getClass().getDeclaredField("dependency1").get(dependency).getClass());
+		assertEquals(dependency2, dependency.getClass().getDeclaredField("dependency2").get(dependency).getClass());
+	}
+	
+	@SneakyThrows
+	@Test
+	void injectTest() {
+		val manager = new DependencyManagerImpl(new HashSetDependencyPool());
+		val injectObject = new ByteBuddy()
+			.subclass(Object.class)
+			.defineField("dependency", Object.class, Modifier.PUBLIC)
+			.annotateField(AnnotationDescription.Builder.ofType(Injection.class).build())
+			.make()
+			.load(this.getClass().getClassLoader())
+			.getLoaded()
+			.newInstance();
+		
+		val dependencyObject = new Object();
+		manager.getDependencyPool().register(dependencyObject, "", Object.class);
+		manager.inject(injectObject);
+		
+		assertEquals(
+			dependencyObject,
+			injectObject.getClass()
+				.getDeclaredField("dependency")
+				.get(injectObject)
+		);
+	}
+	
+}
